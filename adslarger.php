85ff02f63d5bd94da2f6390b2d3b10ee17082a0e
Alade of GOD <michealakintola106@yahoo.coom>
2017-02-18 17:37:53 -0800

    added two columns to ads_table and rewrote select queries of ads

--- adslarger.php
+++ adslarger.php
@@ -2,7 +2,7 @@
 
     require_once ('otun-admin/php/dbfunctions/connect.inc.php');
 
-    $sqlpost = "SELECT * FROM `ads_table` WHERE `id` = '3'";
+    $sqlpost = "SELECT * FROM `ads_table` WHERE `id` = '3' AND `flag` != '0'";
     if(!$result = $db->query($sqlpost)){
         die('There was an error running the query [' . $db->error . ']');
     }
@@ -10,7 +10,7 @@
     while($row = $result->fetch_assoc()){
         echo '
         <a href="'.$row["adslink"].'" target="_blank">
-            <img style="margin: 0 auto;  display: block;"  width="728" height="90" src="'.$row["adslinkimg"].'" alt="Free Hosting" >
+            <img style="margin: 0 auto;  display: block;"  width="728" height="90" src="'.$row["adslinkimg"].'" alt="'.$row["alt"].'" >
         </a>
         ';
     }
--- advert.php
+++ advert.php
@@ -2,21 +2,21 @@
 
     require_once ('otun-admin/php/dbfunctions/connect.inc.php');
 
-    $sqlpost = "SELECT * FROM `ads_table` WHERE `id` != '3'";
+    $sqlpost = "SELECT * FROM `ads_table` WHERE `id` != '3' AND `flag` != '0'";
     if(!$result = $db->query($sqlpost)){
         die('There was an error running the query [' . $db->error . ']');
     }
 
     echo '<div class="vce-sticky">
         <div id="mks_ads_widget-3" class="widget mks_ads_widget">
-            <h4 class="widget-title">Place Ads Here</h4>
+            <h4 class="widget-title">Order For Yours</h4>
             <ul class="mks_adswidget_ul small">';
 
     while($row = $result->fetch_assoc()){
         echo '
             <li data-showind="0">
                 <a href="'.$row["adslink"].'" target="_blank">
-                    <img src="'.$row["adslinkimg"].'" alt="Free Hosting" width="125"  height="125"/>
+                    <img src="'.$row["adslinkimg"].'" alt="'.$row["alt"].'" width="125"  height="125"/>
                 </a>
             </li>
         ';
